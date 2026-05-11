aae286d0190ea9998b9cd585fdf421189f473d62
Veer Shah <vshah4186@gmail.com>
2026-05-11 19:37:50 +0000

    removed old utils file

--- utils.py
+++
@@ -1,8 +0,0 @@
-# This file has been replaced by the utils/ package.
-# All functions are available via: from utils import <function>
-# See utils/__init__.py for the full export list.
-raise ImportError(
-    "utils.py is deprecated. Import from the 'utils' package instead.\n"
-    "Python should auto-prefer the utils/ package over this file.\n"
-    "If you see this error, delete this file manually."
-)
